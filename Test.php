9907274c5434c631f31229fd59707135885aa826
GDownie92 <gdownie92@gmail.com>
2015-10-19 15:31:16 +0100

    test

---
+++ Test.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1110074
+ * Date: 19/10/2015
+ * Time: 15:30
+ */
+
+$numberOfHobbits = 2;
+
+switch ($numberOfHobbits) {
+    case 1:
+        echo "1 sad hobbit";
+        break;
+    case 2:
+        echo "2 happy hobbits";
+        break;
+    case 3:
+        echo "3 hobbits are a crowd";
+        break;
+    default:
+        echo "All the hobbits have gone home";
+}
