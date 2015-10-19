d8bc4e0bcfffd063cc24f25ef8e65df723967d08
GDownie92 <gdownie92@gmail.com>
2015-10-19 15:45:34 +0100

    test

---
+++ Arrays.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1110074
+ * Date: 19/10/2015
+ * Time: 15:44
+ */
+
+$myArray = array("do", "re", "mi"); // declares the array
+
+echo $myArray[0]; // outputs "do"
+
+$myArray[1] = "la"; // modifies position 1 (re)
+
+echo $myArray[1];// outputs "la"
+
+unset($array[2]); // removes the array in position 2
