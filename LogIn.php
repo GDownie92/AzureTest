c2f5755a24f2edef7b4881309fb8bc2b9cb3d323
GDownie92 <gdownie92@gmail.com>
2015-11-02 14:03:53 +0000

    test

---
+++ LogIn.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1110074
+ * Date: 02/11/2015
+ * Time: 13:57
+ */
+
+<form method="post" action="login.php">
+<input type="text" placeholder="username" name="username">
+<input type="password" placeholder="password" name="password">
+<input type="submit" value="Submit">
+</form>
